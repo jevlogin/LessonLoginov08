09a4180fd25f282acd1f334037b32049b69ad7a3
jevlogin <jev.login@mail.ru>
2020-07-27 15:15:20 +0300

    подготовка

---
+++ les02/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Dynamic;
+
+namespace les02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic d = new ExpandoObject();
+            d.result = new int[] { 1, 2, 3, 4 };
+            d.ok = true;
+
+            XElement xElementRoot = new XElement("root");
+
+            XElement xElementResult = new XElement("result");
+
+            foreach (dynamic item in d.result)
+            {
+                XElement xElementResultItem = new XElement("resultItem", item);
+
+                xElementResult.Add(xElementResultItem);
+            }
+
+
+
+            XElement xElementOk = new XElement("ok", d.ok = true);
+
+            xElementRoot.Add(xElementResult, xElementOk);
+            xElementRoot.Save("qwe.xml");
+        }
+    }
+}
---
+++ les03/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace les03
+{
+    [Serializable]
+    public class Student
+    {
+        private int _age;
+        public string FirstName;
+        public string LastName;
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+    }
+    class Program
+    {
+        static void SaveAsXmlFormat(Student obj, string fileName)
+        {
+            XmlSerializer xmlFormat = new XmlSerializer(typeof(Student));
+            Stream fStream = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            xmlFormat.Serialize(fStream, obj);
+            fStream.Close();
+        }
+
+        static Student LoadFromXmlFormat(string fileName)
+        {
+            Student student = new Student();
+            XmlSerializer xmlFormat = new XmlSerializer(typeof(Student));
+            Stream fStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            student = xmlFormat.Deserialize(fStream) as Student;
+            fStream.Close();
+            return student;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
