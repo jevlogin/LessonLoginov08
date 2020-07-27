457f512e1c733aa122cc69b4a5151ae9bfabfded
jevlogin <jev.login@mail.ru>
2020-07-27 13:57:50 +0300

    Учимся парсить json

--- LessonLoginov08/Program.cs
+++ LessonLoginov08/Program.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -11,9 +15,27 @@ namespace LessonLoginov08
     {
         static void Main(string[] args)
         {
-            Form f1 = new Form();
-            Application.Run(f1);
-            MessageBox.Show("Это пока что просто заглушка.");
+            var json = JObject.Parse(new WebClient() { Encoding = Encoding.UTF8 }.DownloadString(@"https://api.telegram.org/bot1001321317:AAFL2MLgX7bdyxNNmrKEVMZ-Hb3HLUk6GOQ/getupdates"));
+            Console.WriteLine(json);
+
+            try
+            {
+                foreach (dynamic item in json)
+                {
+                    Console.WriteLine(item.result);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            dynamic d = new ExpandoObject();
+            d.result = new int[] { 1, 2, 3, 4 };
+            d.ok = true;
+
+            var r = JsonConvert.SerializeObject(d);
+            Console.WriteLine(r);
         }
     }
 }
