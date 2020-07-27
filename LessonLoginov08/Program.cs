0d6a5dfa20b5fccfaba9f71b674fc0dcf84b700e
jevlogin <jev.login@mail.ru>
2020-07-27 12:55:19 +0300

    Добавьте файлы проекта.

---
+++ LessonLoginov08/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LessonLoginov08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form f1 = new Form();
+            Application.Run(f1);
+            MessageBox.Show("Это пока что просто заглушка.");
+        }
+    }
+}
