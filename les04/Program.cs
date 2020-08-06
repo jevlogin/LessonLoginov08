97a70a909d213dda1d448b6e83557f5424e95033
jevlogin <jev.login@mail.ru>
2020-08-06 15:23:56 +0300

    А вот такой метод, подходит для любой программы

--- les04/Program.cs
+++ les04/Program.cs
@@ -18,40 +18,17 @@ namespace les04
     {
         public double Re { get; set; }
         public double Im { get; set; }
+
     }
     class Program
     {
-        static public void SumComplex(Complex a, Complex b)
+        static public Complex SumComplex(Complex a, Complex b)
         {
-            double re, im;
-
-            Console.Write("Введите a.re: ");
-            bool f = double.TryParse(Console.ReadLine(), out re);
-
-            Console.Write("Введите a.im: ");
-            f &= double.TryParse(Console.ReadLine(), out im);
-            if (!f)
+            return new Complex()
             {
-                Console.WriteLine($"Данные кривые");
-                return;
-            }
-
-            a = new Complex() { Re = re, Im = im };
-
-            Console.Write("Введите b.re: ");
-            f = double.TryParse(Console.ReadLine(), out re);
-
-            Console.Write("Введите b.im: ");
-            f &= double.TryParse(Console.ReadLine(), out im);
-            if (!f)
-            {
-                Console.WriteLine($"Данные кривые");
-                return;
-            }
-
-            b = new Complex() { Re = re, Im = im };
-
-            Console.WriteLine($"a + b = {a.Re + b.Re} + {a.Im + b.Im}");
+                Re = a.Re + b.Re,
+                Im = a.Im + b.Im
+            };
         }
         static void Main(string[] args)
         {
