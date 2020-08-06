185642f7cc1fac2a899830f1b07dec38e9719cbf
jevlogin <jev.login@mail.ru>
2020-08-06 15:21:21 +0300

    Для консоли подходит, но не для всех проектов! Потому что мы сейчас азвязаны на Console

---
+++ les04/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/**
+ * 
+ * Разделение логики и интерфейса. Понятие.
+ * 
+ * */
+
+
+namespace les04
+{
+    class Complex
+    {
+        public double Re { get; set; }
+        public double Im { get; set; }
+    }
+    class Program
+    {
+        static public void SumComplex(Complex a, Complex b)
+        {
+            double re, im;
+
+            Console.Write("Введите a.re: ");
+            bool f = double.TryParse(Console.ReadLine(), out re);
+
+            Console.Write("Введите a.im: ");
+            f &= double.TryParse(Console.ReadLine(), out im);
+            if (!f)
+            {
+                Console.WriteLine($"Данные кривые");
+                return;
+            }
+
+            a = new Complex() { Re = re, Im = im };
+
+            Console.Write("Введите b.re: ");
+            f = double.TryParse(Console.ReadLine(), out re);
+
+            Console.Write("Введите b.im: ");
+            f &= double.TryParse(Console.ReadLine(), out im);
+            if (!f)
+            {
+                Console.WriteLine($"Данные кривые");
+                return;
+            }
+
+            b = new Complex() { Re = re, Im = im };
+
+            Console.WriteLine($"a + b = {a.Re + b.Re} + {a.Im + b.Im}");
+        }
+        static void Main(string[] args)
+        {
+            Complex z1 = new Complex();
+            Complex z2 = new Complex();
+
+            SumComplex(z1, z2);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ les04/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("les04")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("les04")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("97e3d6c0-665a-41f9-8ca4-532d60578449")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
