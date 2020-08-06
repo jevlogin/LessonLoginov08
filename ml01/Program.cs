72712bb2ee4ed085f5c7eca0dc132d85f0168f62
jevlogin <jev.login@mail.ru>
2020-08-06 17:39:18 +0300

    штудирую методичку

---
+++ ml01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ml01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Используется для получения объекта System.Type для типа.
+            // Выражение typeof принимает следующую форму:
+
+            Type type = typeof(int);
+            Console.WriteLine(type);
+
+            // Для получения типа выражения во время выполнения можно 
+            // воспользоваться методом платформы.NET GetType, как показано в следующем примере:
+            int i = 0;
+            Type type1 = i.GetType();
+            Console.WriteLine(type1);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ml02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ml02
+{
+    class Program
+    {
+        static PropertyInfo GetPropertyInfo(object obj, string str)
+        {
+            return obj.GetType().GetProperty(str);
+        }
+        static void Main(string[] args)
+        {
+            DateTime dateTime = new DateTime();
+            //dateTime.DayOfWeek
+            Console.WriteLine(GetPropertyInfo(dateTime, "DayOfWeek").CanRead);
+            Console.WriteLine(GetPropertyInfo(dateTime, "DayOfWeek").CanWrite);
+            Console.WriteLine(GetPropertyInfo(dateTime, "DayOfWeek").GetValue(dateTime, null));
+
+            Console.ReadKey();
+        }
+    }
+}
