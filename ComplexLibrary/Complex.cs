21bdaa878359e05d57f8714faaaa72738abcfa91
jevlogin <jev.login@mail.ru>
2020-08-06 15:54:56 +0300

    Продолжаем разделять логику и интерфейс

---
+++ ComplexLibrary/Complex.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexLibrary
+{
+    public class Complex
+    {
+        public double Re { get; set; }
+        public double Im { get; set; }
+
+        static public bool TryParse(string s, out Complex z)
+        {
+            try
+            {
+                var data = s.Split(',');
+                z = new Complex()
+                {
+                    Re = Convert.ToDouble(data[0]),
+                    Im = Convert.ToDouble(data[1])
+                };
+                return true;
+            }
+            catch (Exception)
+            {
+                z = new Complex();
+                return false;
+            }
+        }
+
+        static public Complex SumComplex(Complex a, Complex b)
+        {
+            return new Complex()
+            {
+                Re = a.Re + b.Re,
+                Im = a.Im + b.Im
+            };
+        }
+
+        public override string ToString()
+        {
+            return $"{Re} + {Im}i";
+        }
+    }
+}
--- les04/Program.cs
+++ les04/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ComplexLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,54 +15,17 @@ using System.Threading.Tasks;
 
 namespace les04
 {
-    class Complex
-    {
-        public double Re { get; set; }
-        public double Im { get; set; }
-
-        static public bool TryParse(string s, out Complex z)
-        {
-            try
-            {
-                var data = s.Split(',');
-                z = new Complex()
-                {
-                    Re = Convert.ToDouble(data[0]),
-                    Im = Convert.ToDouble(data[1])
-                };
-                return true;
-            }
-            catch (Exception)
-            {
-                z = new Complex();
-                return false;
-            }
-        }
-
-        public override string ToString()
-        {
-            return $"{Re} + {Im}i";
-        }
-    }
+    
     class Program
     {
-        static public Complex SumComplex(Complex a, Complex b)
-        {
-            return new Complex()
-            {
-                Re = a.Re + b.Re,
-                Im = a.Im + b.Im
-            };
-        }
-
         static void Main(string[] args)
         {
             Complex z1;
-            Console.WriteLine($"Введите 4.5,5.2");
+            Console.Write($"Введите z1 - например, 4.5,5.2: ");
             var f = Complex.TryParse(Console.ReadLine(), out z1);
 
             Complex z2;
-            Console.WriteLine($"Введите 4.5,5.2");
+            Console.Write($"Введите z2 - например, 4.5,5.2: ");
             f &= Complex.TryParse(Console.ReadLine(), out z2);
 
             if (!f)
@@ -71,10 +35,11 @@ namespace les04
 
             if (f)
             {
-                Console.WriteLine(SumComplex(z1, z2));
+                Console.WriteLine(Complex.SumComplex(z1, z2));
             }
 
             Console.ReadKey();
         }
+
     }
 }
